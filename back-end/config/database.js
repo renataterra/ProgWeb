9fbc4db0168c3bfefefd2fc7fe2bc4a4b71dbf5f
RENATA MARIA TERRA SOUSA <1090481823037@fatec.local>
2020-03-04 20:28:08 -0300

    Criando respositório

---
+++ back-end/config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+module.exports = function(uri) {
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    mongoose.connection.on('connected', ()=> 
+    console.log(`mongoose! conectado a${uri}`))
+
+    mongoose.connection.on('disconnected', ()=> 
+    console.log(`mongoose! conectado a${uri}`))
+    
+    mongoose.connection.on('error', erro => 
+    console.log(`Mongoose! ERRO ao conectar a ${uri}: ${erro}`))
+
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            console.log('Mongoose! dseconectado pelo término da aplicação')
+            process.exit(0) // 0 => saída sem erros
+        })
+    })
+
+}
+
---
+++ back-end/controllers/fornecedor.js
@@ -0,0 +1,1 @@
+const fornecedor = require('../models/Fornecedor')
