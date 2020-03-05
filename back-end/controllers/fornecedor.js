e6a8347e8e9a2201712a923eef660bf06f502ee5
RENATA MARIA TERRA SOUSA <1090481823037@fatec.local>
2020-03-04 22:07:08 -0300

    Última atualização - Aula 04/03/2020

--- back-end/controllers/fornecedor.js
+++ back-end/controllers/fornecedor.js
@@ -1,1 +1,18 @@
-const fornecedor = require('../models/Fornecedor')
+const Fornecedor = require('../models/Fornecedor')
+
+const controller = {} // Objeto vazio
+
+controller.novo = async (req, res) => {
+    try {
+        await Fornecedor.create(req.body)
+        // HTTP Status 201: Created
+        res.sendStatus(201)
+    }
+    catch(erro) {
+        console.log(erro)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(erro)
+    }
+}
+
+module.exports = controller
